f1dd33cccc3c939ac06e9636cf52cdd128890000
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2023-12-05 15:44:47 -0600

    retrofit (day 2) to run part one as originally stated

--- 2023/02/app.rb
+++ 2023/02/app.rb
@@ -1,33 +1,79 @@
-def is_possible?(game)
-  game['red'] <= 12 && game['green'] <= 13 && game['blue'] <= 14 && game.keys.size <= 3
-end
-def get_power(game)
-  game.values.inject(:*) 
-end
+class App
 
-sum = 0
+  def initialize(part:)
+    @part = part
+  end
 
-STDIN.each_line do |line|
-  id, draws = /Game (\d+): (.*)/.match(line).captures
-
-  max = {}
-
-  draws.split(';').each do |draw|
-    colors = draw.split(',')
-    colors.each do |cubes|
-      num, color = cubes.chomp.split(' ')
-      num = num.to_i
-
-      if num > max[color].to_i
-        max[color] = num
+  def run(filename)
+    sum = 0
+    File.open(filename) do |f|
+      f.each_line do |line|
+        if @part == 1
+          sum += id_of_possible_game(line) || 0
+        else
+          sum += power_of_min_cubes(line)
+        end
       end
     end
+    sum
   end
 
-  if is_possible?(max)
-  #  sum += id.to_i
+  def id_of_possible_game(line)
+    id, game = str_to_game(line)
+    throws = str_to_throws(game).map {|th| str_to_colors(th) }
+    id if all_throws_possible?(throws)
   end
-  sum += get_power(max)
+
+  def all_throws_possible?(throws)
+    throws.all? {|game| is_game_possible?(game) }
+  end
+
+  def power_of_min_cubes(line)
+    id, game = str_to_game(line)
+    throws = str_to_throws(game).map {|th| str_to_colors(th) }
+    get_power(get_max(throws))
+  end
+
+  def get_max(throws)
+    max = {}
+    throws.each do |game|
+      game.each do |color,value|
+        if (max[color] ||= 0) < value
+          max[color] = value
+        end
+      end
+    end
+    max
+  end
+
+  def get_power(game)
+    game.values.inject(:*) 
+  end
+
+  def is_game_possible?(game)
+    (game['red'] || 0) <= 12 && (game['green'] || 0 ) <= 13 && (game['blue'] || 0) <= 14 && game.keys.size <= 3
+  end
+
+  def str_to_colors(str)
+    h = {}
+    str.split(',').each do |cubes|
+      num, color = cubes.split(' ')
+      num = num.to_i
+      h[color] = num
+    end
+    h
+  end
+
+  def str_to_throws(str)
+    str.split(';').map(&:strip)
+  end
+
+  def str_to_game(str)
+    id, throws = /Game (\d+): (.*)/.match(str).captures
+    return id.to_i, throws
+  end
 end
 
-puts sum
+if __FILE__ == $0
+  puts App.new(part: ENV['PART'].to_i).run ARGV[0]
+end
