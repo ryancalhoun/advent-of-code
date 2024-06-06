a3ba25e9c98f116ce38bc04ff3265c8e6c87cdd7
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2024-06-06 13:16:50 -0500

    2018 day 2, similar IDs

---
+++ 2018/02/Program.cs
@@ -0,0 +1,44 @@
+﻿int Checksum(List<string> IDs) {
+  int two = 0;
+  int three = 0;
+
+  foreach(string ID in IDs) {
+    var g = from c in ID group c by c into w select w.Count();
+
+    if(g.Contains(2)) {
+      ++two;
+    }
+    if(g.Contains(3)) {
+      ++three;
+    }
+  }
+  return two * three;
+}
+
+string FindClosest(List<string> IDs) {
+  for(int i = 0; i + 1 < IDs.Count; ++i) {
+    for(int j = i + 1; j < IDs.Count; ++j) {
+      string common = "";
+
+      for(int k = 0; k < IDs[i].Length; ++k) {
+        if(IDs[i][k] == IDs[j][k])
+          common += IDs[i][k];
+      }
+
+      if(common.Length + 1 == IDs[i].Length)
+        return common;
+    }
+  }
+  throw new IndexOutOfRangeException("no match");
+}
+
+List<string> IDs = new List<string>();
+for(string? line; (line = Console.ReadLine()) != null;) {
+  IDs.Add(line);
+}
+
+if(Environment.GetEnvironmentVariable("PART") == "1") {
+  Console.WriteLine(Checksum(IDs));
+} else {
+  Console.WriteLine(FindClosest(IDs));
+}
---
+++ 2018/02/README.md
@@ -0,0 +1,10 @@
+# Day 2: Inventory Management System
+
+[Full info here](https://adventofcode.com/2018/day/2)
+
+This solution was simple, just list and string iterations with
+some counting.
+
+This puzzle did give me a chance to learn about C#'s querying. 
+Specifically, the keyword query syntax vs the equivalent method
+syntax. It's an interesting animal.
